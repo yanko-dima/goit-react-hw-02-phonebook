a953645e73ca0c3708903d33644372143d3f41c6
Dmytro Yanko <yanko.dmitriy@gmail.com>
2022-07-15 02:20:02 +0300

    start files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Section from 'components/Section';
+import Phonebook from 'components/Phonebook';
+import Contacts from 'components/Contacts';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  render() {
+    // const { contacts, name } = this.state;
+
+    return (
+      <>
+        <Section title={'Phonebook'}>
+          <Phonebook />
+        </Section>
+        <Section title={'Contacts'}>
+          <Contacts />
+        </Section>
+      </>
+    );
+  }
+}
